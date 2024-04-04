54f9d8c465fee43f12ae94713b2490988cf397e7
Zhamangulova Adinai <zhamangulovaadinai@Zhamangulovas-MacBook-Air.local>
2024-04-04 15:08:23 +0600

    bg image

--- components/Products.vue
+++ components/Products.vue
@@ -20,7 +20,7 @@
 
 <style scoped lang="scss">
 .koler {
-    background-image: url('/static/sofa-img\ 1.png');
+    background-image: url('/static/sofa.png');
     background-repeat: no-repeat;
     background-size: contain;
     height: 100vh;
