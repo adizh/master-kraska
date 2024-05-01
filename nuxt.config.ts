395af56e36072b0e0894861d37abb7d0b4ed055b
Zhamangulova Adinai <zhamangulovaadinai@Zhamangulovas-MacBook-Air.local>
2024-05-01 12:53:52 +0600

    fav2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,8 @@ export default defineNuxtConfig({
     head: {
       link: [
         {rel:'icon',
-      href:'/favicon.ico'
+      href:'/favicon.ico',
+      type: 'image/x-icon'
       }
         // {
         //   rel: "stylesheet",
